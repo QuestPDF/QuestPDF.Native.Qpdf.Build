527d8a1bf448bc95a08efe1c8e88a3bc66c6eddf
MarcinZiabek <marcin@ziabek.com>
2026-05-25 15:36:17 +0200

    Extract qPDF API logic to a dedicated QpdfAPI class and update Program.cs accordingly

--- test/Program.cs
+++ test/Program.cs
@@ -1,8 +1,8 @@
-using System.Runtime.InteropServices;
+using QuestPDF.Qpdf;
 
 Environment.CurrentDirectory = AppContext.BaseDirectory;
 
-var libraryVersion = API.GetQpdfVersion();
+var libraryVersion = QpdfAPI.GetQpdfVersion();
 Console.WriteLine(libraryVersion);
 
 RunTest("page_selection_job.json");
@@ -19,34 +19,5 @@ static void RunTest(string jobFile)
     metadata = metadata.Replace("\"", "\\\"").Replace("\n", "\\n").Replace("\r", "");
     job = job.Replace("{{metadata}}", metadata);
 
-    var jobHandle = API.qpdfjob_init();
-    API.qpdfjob_initialize_from_json(jobHandle, job);
-    API.qpdfjob_run(jobHandle);
-    API.qpdfjob_cleanup(ref jobHandle);
-}
-
-static class API
-{
-    const string LibraryName = "qpdf";
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr qpdf_get_qpdf_version();
-    
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr qpdfjob_init();
-    
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void qpdfjob_cleanup(ref IntPtr jobHandle);
-    
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int qpdfjob_initialize_from_json(IntPtr jobHandle, string json);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int qpdfjob_run(IntPtr jobHandle);
-    
-    public static string GetQpdfVersion()
-    {
-        var ptr = qpdf_get_qpdf_version();
-        return Marshal.PtrToStringAnsi(ptr);
-    }
+    QpdfAPI.ExecuteJob(job);
 }
